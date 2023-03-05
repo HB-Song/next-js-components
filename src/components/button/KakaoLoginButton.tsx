98822e574f1e3359ad21ca2744ee316c719a74b5
송효범 <hyobum.song@spiceware.io>
2023-03-05 17:09:23 +0900

    카카오 로그인 버튼 구현
    카카오 로그인 디자인 가이드를 적용한 버튼 컴포넌트 구현

---
+++ src/components/button/KakaoLoginButton.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import { ComponentPropsWithoutRef, forwardRef, Ref } from 'react';
+import { Language, Combine, Size } from '../../global-config/globalType';
+import Module from '../../styles/button/kakao-login.module.css';
+
+type KakaoLoginButtonProps = Combine<
+  {
+    buttonSize: number | Size;
+    language?: Language;
+  },
+  ComponentPropsWithoutRef<'button'>
+>;
+
+const getSize = (size: Size) => {
+  const windowWidth: number = window.innerWidth;
+  switch (size) {
+    case 'sm':
+      return windowWidth / 2.5;
+    case 'md':
+      return windowWidth / 2;
+    case 'lg':
+      return windowWidth / 1.25;
+    default:
+      return windowWidth;
+  }
+};
+
+const KakaoLoginButton = forwardRef(
+  ({ buttonSize, language, ...props }: KakaoLoginButtonProps, ref: Ref<HTMLButtonElement>) => {
+    const realSize = Number.isInteger(buttonSize) ? (buttonSize as number) : getSize(buttonSize as Size);
+    const symbolSize = realSize / 11 > 30 ? 30 : realSize / 11;
+    const containerHeight = symbolSize * 3 > 70 ? 70 : symbolSize * 3;
+
+    return (
+      <button className={Module.container} style={{ width: realSize, height: containerHeight }} ref={ref} {...props}>
+        <div className={Module.symbolWrapper}>
+          <Image src="/kakao_symbol.png" alt="kakao symbol" width={symbolSize} height={symbolSize} />
+        </div>
+        <div className={Module.labelWrapper}>{language === 'en' ? 'Login with Kakao' : '카카오 로그인'}</div>
+      </button>
+    );
+  }
+);
+KakaoLoginButton.displayName = 'Kakao Login Button';
+
+export default KakaoLoginButton;
